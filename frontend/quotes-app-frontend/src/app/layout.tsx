9e3454e1c68a0f7dd038805ff032e7af76683720
Preeyaporn Petcharat <benzepeeyaporn@gmail.com>
2025-06-07 22:05:52 +0700

    seo

--- frontend/quotes-app-frontend/src/app/layout.tsx
+++ frontend/quotes-app-frontend/src/app/layout.tsx
@@ -1,11 +1,16 @@
 import { AuthProvider } from "../../context/AuthContext";
 import { Pacifico } from "next/font/google";
-
+import { Metadata } from "next";
 
 const pacifico = Pacifico({
   subsets: ["latin"],
   weight: "400",
 });
+
+export const metadata: Metadata = {
+  title: "Quotes Web",
+  description: "Welcome to Quotes Web - Your source for inspiring quotes.",
+};
 
 export default function RootLayout({
   children,
@@ -14,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/public/logoQuote.png" />
+      </head>
       <body className={`${pacifico.className} bg-[#f4f1eb]`}>
         <AuthProvider>{children}</AuthProvider>
       </body>
